fe56454b24857b4ab9d3b98169703949f9431b77
chakal05 <chakirrasta@gmail.com>
2019-08-25 13:41:13 +0200

    add landing page

---
+++ client/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow-x: hidden;
+  background-color: rgb(248, 248, 248);
+}
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
