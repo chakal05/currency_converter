1d1bb3129f358b97b8400032c6627b5c7ad981a3
chakal05 <chakirrasta@gmail.com>
2019-08-26 09:43:11 +0200

    landing page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <landingPage msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import landingPage from "./components/landingPage.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    landingPage
   }
 };
 </script>
