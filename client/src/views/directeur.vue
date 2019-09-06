f7c27a6d859af86d1229f88f506d69a57ec4b9b2
chakal05 <chakirrasta@gmail.com>
2019-09-06 22:45:29 +0200

    add method test

--- client/src/views/directeur.vue
+++ client/src/views/directeur.vue
@@ -173,6 +173,53 @@
               </template>
 
 
+              <template v-if="organigramme">
+
+ <v-card
+    class="mx-auto"
+    max-width="434"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">Marcus Obrien</v-list-item-title>
+              <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+
+              </template>
+
+
             </v-flex>
           </v-layout>
         </v-container>
@@ -187,11 +234,13 @@ export default {
   props: {
     source: String
   },
+
   data: () => ({
     dialog: false,
     drawer: null,
     first : true,
-
+organigramme: false ,
+//TOdo find a way to add methods to the 'items' so that on click something happens 
 
 
     items: [
@@ -400,6 +449,12 @@ export default {
       }
     ]
   }),
+  methods : {
+
+    organigramme() {
+      this.data.items[1].organigramme = true;
+    }
+  },
   mounted() {
     this.$refs.calendar.scrollToTime("08:00");
   }
