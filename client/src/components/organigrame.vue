8704175f674c6b4415633a04f323d03ea74bb867
chakal05 <chakirrasta@gmail.com>
2019-09-11 15:38:55 +0200

    organigramme retrived dynamically

--- client/src/components/organigrame.vue
+++ client/src/components/organigrame.vue
@@ -1,284 +1,164 @@
 <template>
   <v-container>
-    <v-row class="firstRow">
 
-      <v-card class="mx-auto" max-width="250">
-        <v-img
-          class="white--text"
-          height="200px"
-          src="../assets/adri.jpeg"
-        >
-           </v-img>
+<template>
 
-        <v-card-text>
-         
-          <span class="text--primary">
-            <span>Moussa Souleiman</span>
-            <br />
-            <span>Directeur</span>
-          </span>
-        </v-card-text>
+   <v-item-group>
+        <v-container>
+          <v-row>
+            <v-item v-for="(item, i) in direction" :key="i">
+              <v-card class="mx-auto" max-width="250">
+                <v-img
+                  class="white--text"
+                  height="200px"
+                  v-bind:src="require('../assets/' + item.source)"
+                ></v-img>
 
-    <v-card-actions>
-      <v-btn
-        text
-        color="orange"
-      >
-        contacter
-      </v-btn>
-      <v-btn
-        text
-        color="orange"
-      >
-        profil
-      </v-btn>
-    </v-card-actions>
-      </v-card>
-    </v-row>
+                <v-card-text>
+                  <span class="text--primary">
+                    <span v-text="item.nom">Moussa Souleiman</span>
+                    <br />
+                    <span v-text="item.poste"></span>
+                  </span>
+                </v-card-text>
 
-    <v-row class="secondRow">
-      <v-card class="mx-auto" max-width="230">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/directrice.jpeg"
-        >
-         </v-img>
-
-        <v-card-text>
-          <span class="text--primary">
-            <span>Amina Ahmed</span>
-            <br />
-            <span>Directrice pédagogique</span>
-          </span>
-        </v-card-text>
-<v-card-actions>
-      <v-btn
-        text
-        color="orange"
-      >
-        contacter
-      </v-btn>
-      <v-btn
-        text
-        color="orange"
-      >
-        profil
-      </v-btn>
-    </v-card-actions>
+                <v-card-actions>
+                  <v-btn text color="orange">contacter</v-btn>
+                  <v-btn text color="orange">profil</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-item>
+          </v-row>
+        </v-container>
+      </v-item-group>
+</template>
       
-      </v-card>
+     
 
-        <v-card class="mx-auto" max-width="230">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/responsable.jpeg"
-        >
-        </v-img>
 
-        <v-card-text>
-          <span class="text--primary">
-            <span>Halima Mohamed</span>
-            <br />
-            <span>Responsable vie scolaire </span>
-          </span>
-        </v-card-text>
+     <template >
 
-        <v-card-actions>
-          <v-btn text color="orange">contacter</v-btn>
-          <v-btn text color="orange">profil</v-btn>
-        </v-card-actions>
-      </v-card>
-
-        <v-card class="mx-auto" max-width="230">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/comptable.png"
-        >
-         </v-img>
-
-        <v-card-text>
-          <span class="text--primary">
-            <span>Salah Ali </span>
-            <br />
-            <span>Comptable</span>
-          </span>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn text color="orange">Contacter</v-btn>
-          <v-btn text color="orange">Profil</v-btn>
-        </v-card-actions>
-      </v-card>
-
+  <v-item-group class='secondRow'>
+    <v-container>
+      <v-row>
+        <v-item v-for="(item, i) in sousDirection" :key="i">
           <v-card class="mx-auto" max-width="230">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/securite.jpeg"
-        >
-          </v-img>
+            <v-img
+              class="white--text"
+              height="200px"
+              v-bind:src="require('../assets/' + item.source)"
+            ></v-img>
 
-        <v-card-text>
-          <span class="text--primary">
-            <span>Jamal Muhyadin</span>
-            <br />
-            <span>Securite et jardinnage</span>
-          </span>
-        </v-card-text>
+            <v-card-text>
+              <span class="text--primary">
+                <span v-text="item.nom"></span>
+                <br />
+                <span v-text="item.poste"></span>
+              </span>
+            </v-card-text>
 
-        <v-card-actions>
-          <v-btn text color="orange">Contacter</v-btn>
-          <v-btn text color="orange">profil </v-btn>
-        </v-card-actions>
-      </v-card>
+            <v-card-actions>
+              <v-btn text color="orange">contacter</v-btn>
+              <v-btn text color="orange">profil</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-item>
+      </v-row>
+    </v-container>
+  </v-item-group>
+     </template>
 
-    </v-row>
 
-      <v-row class="secondRow">
-      <v-card class="mx-auto" max-width="200">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/surveillante.jpeg"
-        >
-         </v-img>
-
-        <v-card-text>
-          <span class="text--primary">
-            <span>Hanna Moussa </span>
-            <br />
-            <span>Surveillante</span>
-          </span>
-        </v-card-text>
-<v-card-actions>
-      <v-btn
-        text
-        color="orange"
-      >
-        contacter
-      </v-btn>
-      <v-btn
-        text
-        color="orange"
-      >
-        profil
-      </v-btn>
-    </v-card-actions>
-      
-      </v-card>
-
-        <v-card class="mx-auto" max-width="200">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/surveillant.jpeg"
-        >
-        </v-img>
-
-        <v-card-text>
-          <span class="text--primary">
-            <span>Moumin Noor</span>
-            <br />
-            <span>Surveillant  </span>
-          </span>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn text color="orange">contacter</v-btn>
-          <v-btn text color="orange">profil</v-btn>
-        </v-card-actions>
-      </v-card>
-
-        <v-card class="mx-auto" max-width="200">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/secretaire.jpeg"
-        >
-         </v-img>
-
-        <v-card-text>
-          <span class="text--primary">
-            <span>Ifrah Hassan </span>
-            <br />
-            <span>Secretaire</span>
-          </span>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn text color="orange">Contacter</v-btn>
-          <v-btn text color="orange">Profil</v-btn>
-        </v-card-actions>
-      </v-card>
-
+    
+  <template>
+     <v-item-group class='secondRow'>
+    <v-container>
+      <v-row>
+        <v-item v-for="(item, i) in  sousSousDirection" :key="i">
           <v-card class="mx-auto" max-width="200">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/survellant1.png"
-        >
-          </v-img>
+            <v-img
+              class="white--text"
+              height="200px"
+              v-bind:src="require('../assets/' + item.source)"
+            ></v-img>
 
-        <v-card-text>
-          <span class="text--primary">
-            <span>Samatar Salah</span>
-            <br />
-            <span>Surveillant</span>
-          </span>
-        </v-card-text>
+            <v-card-text>
+              <span class="text--primary">
+                <span v-text="item.nom"></span>
+                <br />
+                <span v-text="item.poste"></span>
+              </span>
+            </v-card-text>
 
-        <v-card-actions>
-          <v-btn text color="orange">Contacter</v-btn>
-          <v-btn text color="orange">profil </v-btn>
-        </v-card-actions>
-      </v-card>
+            <v-card-actions>
+              <v-btn text color="orange">contacter</v-btn>
+              <v-btn text color="orange">profil</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-item>
+      </v-row>
+    </v-container>
+  </v-item-group>
+  </template>
 
-
-          <v-card class="mx-auto" max-width="200">
-        <v-img
-          class="white--text"
-          height="250px"
-          src="../assets/surveillante1.jpeg"
-        >
-          </v-img>
-
-        <v-card-text>
-          <span class="text--primary">
-            <span>Kadra Youssouf</span>
-            <br />
-            <span>Surveillante</span>
-          </span>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn text color="orange">Contacter</v-btn>
-          <v-btn text color="orange">profil </v-btn>
-        </v-card-actions>
-      </v-card>
-
-    </v-row>
+    
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-   
-  }),
+    direction: [
+      { source: "adri.jpeg", nom: "Moussa Souleiman", poste: "Directeur" },
+      {
+        source: "directAd.jpeg",
+        nom: "Mouna Ragueh",
+        poste: "Directrice Adjointe"
+      }
+    ],
 
-  methods: () => ({})
+    sousDirection: [
+      {
+        source: "directrice.jpeg",
+        nom: "Amina Ahmed",
+        poste: "Directrice pedagogique"
+      },
+      {
+        source: "responsable.jpeg",
+        nom: "Halima Mohamed",
+        poste: "Responsable vie scolaire"
+      },
+      { source: "comptable.png", nom: "Salah Ali ", poste: "Comptable" },
+      {
+        source: "securite.jpeg",
+        nom: "Jamal Muhyadin ",
+        poste: "Securite et jardinnage"
+      }
+    ],
+
+    sousSousDirection: [
+      {
+        source: "surveillante.jpeg",
+        nom: "Hanna Moussa",
+        poste: "Surveillante"
+      },
+      { source: "surveillant.jpeg", nom: "Moumin Noor", poste: "Surveillant" },
+      { source: "secretaire.jpeg", nom: "Ifrah Hassan ", poste: "Secretaire" },
+      { source: "survellant1.png", nom: "Samatar Salah", poste: "Surveillant" },
+      {
+        source: "surveillante1.jpeg",
+        nom: "Kadra Youssouf",
+        poste: "Surveillante"
+      }
+    ]
+  })
 };
 </script>
 
 <style lang="scss" scoped>
 
-
-.secondRow {
-  .v-card{
-    margin-top: 3rem;
+  .v-card {
+    margin-top: 1rem;
   }
-}
+
 </style>
