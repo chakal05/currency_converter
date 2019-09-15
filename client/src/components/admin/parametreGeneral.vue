db4a9f6a7e78d224af840da5f4f9b75d0cbde0cf
chakal05 <chakirrasta@gmail.com>
2019-09-15 17:34:30 +0200

    get choosen color for customizing v-app

--- client/src/components/admin/parametreGeneral.vue
+++ client/src/components/admin/parametreGeneral.vue
@@ -1,35 +1,54 @@
-
 <template>
-  <div class="d-flex justify-center">
-    <v-color-picker
-      :hide-canvas="true"
-      :hide-inputs="true"
-      class="ma-2"
-      show-swatches
-      swatches-max-height="300px"
-     @click="getColor"
-    ></v-color-picker>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col class="d-flex justify-center">
+        <v-color-picker :show-swatches="true" :hide-canvas="true"
+        :hide-inputs="true" swatches-max-height="300px"
+         v-model="color"></v-color-picker>
+         <v-btn color='primary' @click="fam"> Choosen color  </v-btn>
+      </v-col>
+     
+    </v-row>
+  </v-container>
 </template>
-
 <script>
 export default {
   data: () => ({
-    swatches: [
-      ["#FF0000", "#AA0000", "#550000"],
-      ["#FFFF00", "#AAAA00", "#555500"],
-      ["#00FF00", "#00AA00", "#005500"],
-      ["#00FFFF", "#00AAAA", "#005555"],
-      ["#0000FF", "#0000AA", "#000055"]
-    ]
+    type: "hex",
+    hex: "#FF00FF",
+    couleur: ''
   }),
 
-  methods: () => ({
-    getColor: function() {
-      let item = document.querySelector('.v-color-picker__color');
-  
+  computed: {
+    color: {
+      get() {
+        return this[this.type];
+      },
+      set(v) {
+        this[this.type] = v;
+      }
+    },
+    showColor() {
+      if (typeof this.color === "string") return this.color;
+
+      return JSON.stringify(
+        Object.keys(this.color).reduce((color, key) => {
+          color[key] = Number(this.color[key].toFixed(2))
+         return color;
+        }, {}),
+        null,
+        2
+      );
     }
-  })
+  },
+
+  methods: {
+    fam: function() {
+      if (this.color) {
+        alert(this.color);
+      }
+    }
+  }
 };
 </script>
 
