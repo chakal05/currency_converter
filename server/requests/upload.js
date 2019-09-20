72e1437702cab4955588c20fa0d3eba34200d7d6
chakal05 <chakirrasta@gmail.com>
2019-09-20 18:45:18 +0200

    use file path to avoid conflic

--- server/requests/upload.js
+++ server/requests/upload.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const multer = require('multer');
 
 const customLogo = multer({
-    dest : "../logosFolder"
+    dest : "./logosFolder"
 });
 
-router.post('/', customLogo.single("file"), (req, res) => {
-    res.json({file : req.file});
-    console.log(req.file);
+router.post('/', upload = customLogo.single("file"), (req, res) => {
+    
+    // req.file.path should be used to retrive schools logos
+
+
+    if(upload){
+    res.json({status : req.file.filename});
+    }
+ 
 })
 
 
