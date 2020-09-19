9788d973fe366bf889c468574106fe166b3f958f
chakal05 <chakirrasta@gmail.com>
2020-09-19 23:06:10 +0200

    prepare to deploy

---
+++ build/precache-manifest.db87d942a0fe2a1908c75453b2405124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b695ffb6c410549081c7c2a12d899bb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5a986cf0e4be8954391",
+    "url": "/static/js/2.b1445f58.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b1445f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1668fa39d58b73ac6214",
+    "url": "/static/js/main.46c8f98f.chunk.js"
+  },
+  {
+    "revision": "1f2547e8b92be90d5f4a",
+    "url": "/static/js/runtime-main.ec592ac5.js"
+  }
+]);
